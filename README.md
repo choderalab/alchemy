a5508fbd03853c51bbbbc357c4fee05d8713447e
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-09-21 17:49:29 +0100

    Added setup.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/choderalab/alchemy.svg?branch=master)](https://travis-ci.org/choderalab/alchemy)
+
 # Alchemical tools for OpenMM
 
 
---
+++ setup.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""alchemy: Alchemical tools for OpenMM
+"""
+
+from __future__ import print_function, absolute_import
+
+DOCLINES = __doc__.split("\n")
+
+import os
+import sys
+import glob
+import traceback
+import numpy as np
+from os.path import join as pjoin
+from os.path import relpath
+from setuptools import setup, Extension, find_packages
+try:
+    sys.dont_write_bytecode = True
+    sys.path.insert(0, '.')
+    from basesetup import write_version_py, CompilerDetection, check_dependencies
+finally:
+    sys.dont_write_bytecode = False
+
+
+if '--debug' in sys.argv:
+    sys.argv.remove('--debug')
+    DEBUG = True
+else:
+    DEBUG = False
+
+#Useful function
+def find_package_data(data_root, package_root):
+    files = []
+    for root, dirnames, filenames in os.walk(data_root):
+        for fn in filenames:
+            files.append(relpath(pjoin(root, fn), package_root))
+    return files
+
+
+# #########################
+VERSION = '1.0.0'
+ISRELEASED = True
+__version__ = VERSION
+# #########################
+
+CLASSIFIERS = """\
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)
+Programming Language :: C++
+Programming Language :: Python
+Development Status :: 4 - Beta
+Topic :: Software Development
+Topic :: Scientific/Engineering
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: MacOS
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+"""
+
+extensions = []
+
+setup(name='alchemy',
+      author='John D. Chodera',
+      author_email='john.chodera@choderalab.org',
+      description=DOCLINES[0],
+      long_description="\n".join(DOCLINES[2:]),
+      version=__version__,
+      url='https://github.com/choderalab/alchemy',
+      platforms=['Linux', 'Mac OS-X', 'Unix'],
+      classifiers=CLASSIFIERS.splitlines(),
+      packages=['alchemy', 'alchemy.tests',],
+      #package_data={'alchemy' : find_package_data('alchemy','examples')},
+      zip_safe=False,
+      ext_modules=extensions,
+      install_requires=[
+        'openmm >=6.3',
+        'numpy',
+        'scipy',
+        'openmmtools',
+        ],
+      )
